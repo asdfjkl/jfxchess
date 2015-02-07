2ddf9e7cc4f6a0bf30cb7a273c3156adaab76815
asdfjkl <asdfjkl@users.noreply.github.com>
2015-02-07 19:35:49 +0100

    setup
    Former-commit-id: c739f6ce2ca5067e5e6edcebe6410f7e9c1c094e

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ OPTIONS = {'argv_emulation': True, 'iconfile': 'icon.icns'}
 
 setup(
     app=APP,
-    name='Jerry'
+    name='Jerry',
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
