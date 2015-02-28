ac53cfc6c1585312f62404fa8d4b47aa7812ac67
asdfjkl <asdfjkl@thinkpad>
2015-02-28 18:04:04 +0100

    added __init__.py for util (otherwise module not found with py2exe)
    Former-commit-id: 2b958a633cf6df57dccba248fe5dcf9806051140

--- util/proc.py
+++ util/proc.py
@@ -1,3 +1,5 @@
+import os
+
 def set_lowpriority(pid):
     """ Set the priority a process with pid to below-normal."""
 
@@ -13,10 +15,15 @@ def set_lowpriority(pid):
         # Based on:
         #   "Recipe 496767: Set Process Priority In Windows" on ActiveState
         #   http://code.activestate.com/recipes/496767/
-        import win32api,win32process,win32con
 
-        handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
-        win32process.SetPriorityClass(handle, win32process.BELOW_NORMAL_PRIORITY_CLASS)
+        #import win32api,win32process,win32con
+        #print("pid: "+str(pid))
+        #handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
+        #win32process.SetPriorityClass(handle, win32process.BELOW_NORMAL_PRIORITY_CLASS)
+
+        # currently. on Windows qprocess.pid() returns a void pointer, and not the process id
+        # hence, on Windows this is not implemented until finding a proper workaround
+        pass
     else:
         import os
         os.system('renice -n 1 -p '+str(pid))
