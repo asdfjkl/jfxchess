80a1a307103ac6a32476441ec50ea4c3322d7e81
asdfjkl <asdfjkl@thinkpad>
2015-03-13 23:29:59 +0100

    set window title in messagebox
    Former-commit-id: ea1448f579b9b90ed826b3767b95648693d47f1e

--- util/messages.py
+++ util/messages.py
@@ -2,7 +2,8 @@ from PyQt4.QtGui import QMessageBox
 
 def display_mbox(title,content):
     msgBox = QMessageBox()
+    msgBox.setWindowTitle("Jerry")
     msgBox.setIcon(QMessageBox.Information)
-    msgBox.setText(title+content)
+    msgBox.setText(title+"\n"+content)
     #msgBox.setInformativeText(content)
     msgBox.exec_()
