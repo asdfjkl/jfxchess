16378f38c9dfc8f575ce2a99d8a44cc29fd2996e
asdfjkl <asdfjkl@users.noreply.github.com>
2015-03-13 23:12:50 +0100

    latest
    Former-commit-id: d34ab9e32fbd7a3aa668417512923aa76c1cb89f

--- util/messages.py
+++ util/messages.py
@@ -3,6 +3,6 @@ from PyQt4.QtGui import QMessageBox
 def display_mbox(title,content):
     msgBox = QMessageBox()
     msgBox.setIcon(QMessageBox.Information)
-    msgBox.setText(title)
-    msgBox.setInformativeText(content)
+    msgBox.setText(title+content)
+    #msgBox.setInformativeText(content)
     msgBox.exec_()
