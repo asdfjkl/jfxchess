69b28bc2343152fae2e1e34e6dddb5cf840ad2ee
asdfjkl <asdfjkl@users.noreply.github.com>
2015-04-23 22:13:04 +0200

    translation groundwork done
    Former-commit-id: ed44f6fca0b86b52e5734c7634f63a5e801353b0

--- jerry.py
+++ jerry.py
@@ -22,7 +22,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        print(self.tr("foobar"))
+        print(self.trUtf8("foobar"))
 
         self.resize(800, 470)
         self.setWindowTitle('Jerry - Chess')
