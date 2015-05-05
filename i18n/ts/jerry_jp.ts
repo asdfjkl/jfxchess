d08898a49e1c9e49ed30cab4601b13033b2bc252
asdfjkl <asdfjkl@users.noreply.github.com>
2015-05-05 20:29:57 +0200

    translations

---
+++ i18n/ts/jerry_jp.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="ja_JP" sourcelanguage="en_US">
+<context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="dialogs/DialogAbout.py" line="22"/>
+        <source>&lt;b&gt;Jerry&lt;/b&gt;&lt;br&gt;&lt;br&gt;Version 1.02&lt;br&gt;&lt;br&gt;Copyright &#xc2;&#xa9; 2014, 2015 Dominik Klein&lt;br&gt;licensed under GNU GPL 3&lt;br&gt;&lt;br&gt;&lt;b&gt;Credits&lt;/b&gt;&lt;br&gt;&lt;br&gt;Stockfish Chess Engine&lt;br&gt;by the Stockfish-Team&lt;br&gt;&lt;br&gt;python-chess library&lt;br&gt;by Niklas Fiekas&lt;br&gt;&lt;br&gt;&apos;VARIED.BIN&apos; opening book&lt;br&gt;by Heinz van Saanen&lt;br&gt;&lt;br&gt;Merida piece images&lt;br&gt;from pychess/pychess-team&lt;br&gt;&lt;br&gt;all licensed under GNU GPL 3</source>
+        <translation>よし</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="jerry.py" line="25"/>
+        <source>foobar</source>
+        <translation type="obsolete">よかった</translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="31"/>
+        <source>Exit application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="100"/>
+        <source>File </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="101"/>
+        <source>New Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="105"/>
+        <source>Open Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="108"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="113"/>
+        <source>Save as ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="116"/>
+        <source>Append to ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="119"/>
+        <source>Save Position as Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="122"/>
+        <source>Print Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="125"/>
+        <source>Print Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="128"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="133"/>
+        <source>Edit </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="134"/>
+        <source>Copy Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="137"/>
+        <source>Copy Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="139"/>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="143"/>
+        <source>&amp;Enter Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="147"/>
+        <source>Edit Game Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="149"/>
+        <source>&amp;Flip Board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="152"/>
+        <source>Show Search &amp;Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="158"/>
+        <source>Offer Draw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="161"/>
+        <source>Resign</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="167"/>
+        <source>Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="169"/>
+        <source>&amp;Analysis Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="173"/>
+        <source>Play as &amp;White</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="178"/>
+        <source>Play as &amp;Black</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="183"/>
+        <source>Enter &amp;Moves</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="190"/>
+        <source>Full Game Analysis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="193"/>
+        <source>Play out Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="198"/>
+        <source>Strength Level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="204"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>m_help</name>
+</context>
+</TS>
