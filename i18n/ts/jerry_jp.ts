08eea4a8e8bf0cfed0562c0cfb8e82c01944e899
asdfjkl <asdfjkl@users.noreply.github.com>
2015-05-09 19:42:05 +0200

    latest
    Former-commit-id: 4ef4b8e1d183c37f93cba1a4710da6bf8dc60150

--- i18n/ts/jerry_jp.ts
+++ i18n/ts/jerry_jp.ts
@@ -1,16 +1,242 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="ja_JP" sourcelanguage="en_US">
+<!DOCTYPE TS><TS version="2.0" language="ja_JP" sourcelanguage="en_US">
 <context>
+    <name>ChessboardView</name>
+    <message>
+        <location filename="gui/ChessBoardView.py" line="154"/>
+        <source>Checkmate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/ChessBoardView.py" line="154"/>
+        <source>The game is over!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/ChessBoardView.py" line="157"/>
+        <source>Stalemate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/ChessBoardView.py" line="163"/>
+        <source>The game is drawn!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/ChessBoardView.py" line="160"/>
+        <source>Insufficient material to win.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/ChessBoardView.py" line="163"/>
+        <source>Threefold repetition.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context encoding="UTF-8">
     <name>DialogAbout</name>
     <message>
         <location filename="dialogs/DialogAbout.py" line="10"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
+    <message encoding="UTF-8">
         <location filename="dialogs/DialogAbout.py" line="22"/>
         <source>&lt;b&gt;Jerry&lt;/b&gt;&lt;br&gt;&lt;br&gt;Version 1.02&lt;br&gt;&lt;br&gt;Copyright © 2014, 2015 Dominik Klein&lt;br&gt;licensed under GNU GPL 3&lt;br&gt;&lt;br&gt;&lt;b&gt;Credits&lt;/b&gt;&lt;br&gt;&lt;br&gt;Stockfish Chess Engine&lt;br&gt;by the Stockfish-Team&lt;br&gt;&lt;br&gt;python-chess library&lt;br&gt;by Niklas Fiekas&lt;br&gt;&lt;br&gt;&apos;VARIED.BIN&apos; opening book&lt;br&gt;by Heinz van Saanen&lt;br&gt;&lt;br&gt;Merida piece images&lt;br&gt;from pychess/pychess-team&lt;br&gt;&lt;br&gt;all licensed under GNU GPL 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogAnalyzeGame</name>
+    <message>
+        <location filename="dialogs/DialogAnalyzeGame.py" line="14"/>
+        <source>Full Game Analysis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogAnalyzeGame.py" line="16"/>
+        <source>Sec(s) per move:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogAnalyzeGame.py" line="17"/>
+        <source>Threshold (in pawns):</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogEditGameData</name>
+    <message>
+        <location filename="dialogs/DialogEditGameData.py" line="8"/>
+        <source>Edit Game Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogEnterPosition</name>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="181"/>
+        <source>Enter Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="192"/>
+        <source>White O-O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="193"/>
+        <source>White O-O-O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="194"/>
+        <source>Black O-O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="195"/>
+        <source>Black O-O-O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="196"/>
+        <source>Castling Rights</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="205"/>
+        <source>White To Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="206"/>
+        <source>Black To Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="207"/>
+        <source>Turn</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="214"/>
+        <source>Initial Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="215"/>
+        <source>Clear Board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogEnterPosition.py" line="216"/>
+        <source>Current Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogNewGame</name>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="8"/>
+        <source>New Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="10"/>
+        <source>White</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="11"/>
+        <source>Black</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="14"/>
+        <source>Choose your side:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="24"/>
+        <source>Computer Strength</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="36"/>
+        <source>Computer&apos;s Time per Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="38"/>
+        <source>3 sec(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="40"/>
+        <source>20 sec(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogNewGame.py" line="111"/>
+        <source> sec(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogPromotion</name>
+    <message>
+        <location filename="dialogs/DialogPromotion.py" line="10"/>
+        <source>Promotion</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogStrengthLevel</name>
+    <message>
+        <location filename="dialogs/DialogStrengthLevel.py" line="8"/>
+        <source>Set Strength Level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogStrengthLevel.py" line="10"/>
+        <source>Computer Strength</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogStrengthLevel.py" line="22"/>
+        <source>Computer&apos;s Time per Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogStrengthLevel.py" line="24"/>
+        <source>3 sec(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogStrengthLevel.py" line="26"/>
+        <source>20 sec(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs/DialogStrengthLevel.py" line="86"/>
+        <source> sec(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogWithListView</name>
+    <message>
+        <location filename="dialogs/DialogWithListView.py" line="9"/>
+        <source>Next Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogWithPlainText</name>
+    <message>
+        <location filename="dialogs/DialogWithPlaintext.py" line="8"/>
+        <source>Edit Comment</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -164,6 +390,189 @@
     <message>
         <location filename="jerry.py" line="204"/>
         <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="jerry.py" line="203"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MovesEdit</name>
+    <message>
+        <location filename="gui/MovesEdit.py" line="22"/>
+        <source>Move Annotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="23"/>
+        <source>?? Blunder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="25"/>
+        <source>? Mistake</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="27"/>
+        <source>?! Dubious Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="29"/>
+        <source>!? Interesting Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="31"/>
+        <source>! Good Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="33"/>
+        <source>!! Brilliant Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="58"/>
+        <source>No Annotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="39"/>
+        <source>Position Annotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="gui/MovesEdit.py" line="40"/>
+        <source>∞ Unclear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="gui/MovesEdit.py" line="42"/>
+        <source>=/∞ With Compensation for White</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="gui/MovesEdit.py" line="44"/>
+        <source>∞/= With Compensation for Black</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="gui/MovesEdit.py" line="46"/>
+        <source>⩲ White Slightly Better</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="gui/MovesEdit.py" line="48"/>
+        <source>⩱ Black Slightly Better</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="gui/MovesEdit.py" line="50"/>
+        <source>± White Better</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="gui/MovesEdit.py" line="52"/>
+        <source>∓ Black Better</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="54"/>
+        <source>+- White Much Better</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="56"/>
+        <source>-+ Black Much Better</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="146"/>
+        <source>Add/Edit Comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="63"/>
+        <source>Delete Comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="67"/>
+        <source>Remove Annotations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="70"/>
+        <source>Move Variant Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="72"/>
+        <source>Move Variant Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="74"/>
+        <source>Delete Variant</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="76"/>
+        <source>Delete From Here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="79"/>
+        <source>Delete All Comments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/MovesEdit.py" line="81"/>
+        <source>Delete All Variants</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mainWidget</name>
+    <message>
+        <location filename="logic/file_io.py" line="59"/>
+        <source>Save PGN</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mainWindow.self</name>
+    <message>
+        <location filename="logic/file_io.py" line="86"/>
+        <source>Initializing Game Tree</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic/file_io.py" line="103"/>
+        <source>Open PGN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic/gameevents.py" line="353"/>
+        <source>Game Analysis Finished</source>
+        <comment>The analysis is finished.</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>q_widget</name>
+    <message>
+        <location filename="logic/file_io.py" line="25"/>
+        <source>Save Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic/file_io.py" line="33"/>
+        <source>Append to PGN</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- jerry.pro
+++ jerry.pro
@@ -1,2 +1,11 @@
-SOURCES = jerry.py dialogs/DialogAbout.py
-TRANSLATIONS = i18n/ts/jerry_jp.ts
+SOURCES = jerry.py dialogs/DialogAbout.py dialogs/DialogAnalyzeGame.py \
+          dialogs/DialogEditGameData.py dialogs/DialogEnterPosition.py \
+          dialogs/DialogNewGame.py dialogs/DialogPromotion.py          \
+          dialogs/DialogStrengthLevel.py dialogs/DialogWithListView.py \
+          dialogs/DialogWithPlaintext.py                               \
+          gui/ChessBoardView.py gui/GUIPrinter.py gui/MovesEdit.py     \
+          gui/PieceImages.py logic/edit.py logic/file_io.py            \
+          logic/gameevents.py logic/gamestate.py uci/engine_info.py    \
+          uci/uci_controller.py uci/uci_worker.py util/appdirs.py      \
+          util/cache.py util/messages.py util/proc.py                 
+TRANSLATIONS = i18n/ts/jerry_de.ts
