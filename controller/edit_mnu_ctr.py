e8d988f2c9c24f431b120be1b9003bf344453dc5
asdfjkl <asdfjkl@users.noreply.github.com>
2015-08-23 17:46:38 +0200

    various
    Former-commit-id: a02803919cc9e441447e6c0a7a16a9321a9ab8fd

--- controller/edit_mnu_ctr.py
+++ controller/edit_mnu_ctr.py
@@ -8,13 +8,12 @@ from dialogs.dialog_enter_position import DialogEnterPosition
 
 class EditMenuController():
 
-    def __init__(self, mainAppWindow, model):
+    def __init__(self, mainAppWindow):
         super(EditMenuController, self).__init__()
         self.mainAppWindow = mainAppWindow
-        self.model = model
+        self.model = mainAppWindow.model
 
     def copy_game_to_clipboard(self):
-        print("called game to clipboard")
         clipboard = QApplication.clipboard()
         exporter = chess.pgn.StringExporter()
         self.model.gamestate.current.root().export(exporter, headers=True, variations=True, comments=True)
@@ -48,41 +47,36 @@ class EditMenuController():
                 gamestate.current = first_game
                 self.mainAppWindow.setLabels()
                 self.mainAppWindow.save.setEnabled(False)
-                self.model.gamestate.init_game_tree(self.mainAppWindow)
+                gamestate.init_game_tree(self.mainAppWindow)
         boardview.update()
         boardview.emit(SIGNAL("statechanged()"))
 
 
 
     def enter_position(self):
-        mainWindow = self.mainAppWindow
-        dialog = DialogEnterPosition(mainWindow.model.gamestate.current.board())
+        dialog = DialogEnterPosition(self.model.gamestate.current.board())
         answer = dialog.exec_()
         if(answer):
             root = chess.pgn.Game()
             root.headers["FEN"] = ""
             root.headers["SetUp"] = ""
             root.setup(dialog.displayBoard.board)
-            mainWindow.model.gamestate.current = root
-            mainWindow.model.gamestate.initialize_headers()
-            mainWindow.setLabels()
-            mainWindow.chessboard_view.on_statechanged()
-            mainWindow.moves_edit_view.on_statechanged()
-            mainWindow.save.setEnabled(False)
-            mainWindow.update()
+            self.mainAppWindow.model.gamestate.current = root
+            self.mainAppWindow.model.gamestate.initialize_headers()
+            self.mainAppWindow.setLabels()
+            self.mainAppWindow.chessboard_view.on_statechanged()
+            self.mainAppWindow.moves_edit_view.on_statechanged()
+            self.mainAppWindow.save.setEnabled(False)
+            self.mainAppWindow.update()
 
     def reset_to_initial(self):
-
         root = chess.pgn.Game()
         root.headers["FEN"] = ""
         root.headers["SetUp"] = ""
-        #root.setup(dialog.displayBoard.board)
-        self.mainAppWindow.model.gamestate.current = root
-        self.mainAppWindow.model.gamestate.initialize_headers()
+        self.model.gamestate.current = root
+        self.model.gamestate.initialize_headers()
         self.mainAppWindow.setLabels()
         self.mainAppWindow.chessboard_view.on_statechanged()
         self.mainAppWindow.moves_edit_view.on_statechanged()
         self.mainAppWindow.save.setEnabled(False)
         self.mainAppWindow.update()
-        #self.model.gamestate.current.board().reset()
-        #self.mainAppWindow.chessboard_view.update()
