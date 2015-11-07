6ab611705f77c743060fe7bca20a64edc46462f0
asdfjkl <asdfjkl@thinkpad>
2015-11-07 17:38:05 +0100

    removed printers and cludge
    Former-commit-id: deeb822361170ec496845b5f75a43a8c0145e934

--- uci/uci_worker.py
+++ uci/uci_worker.py
@@ -24,19 +24,14 @@ class Uci_worker(QObject):
         self.engine_info = EngineInfo()
 
     def process_command(self):
-        print("processing commands...")
         if(self.process.state() == QProcess.NotRunning and not self.command_queue.empty()):
-            print("if1 ...")
             msg = self.command_queue.get()
             if(msg.startswith("start_engine?")):
                 path = msg.split("?")[1]
                 self.process.start(path+"\n")
                 self.engine_info.strength = None
         elif(self.process.state() == QProcess.Running):
-            print("if2 ...")
-            #time.sleep(0.001)
             output = str(self.process.readAllStandardOutput(),"utf-8")
-            print("if2a ...")
             if(not output == ""):
                 self.engine_info.update_from_string(output,self.current_fen)
                 self.emit(SIGNAL("info(PyQt_PyObject)"),deepcopy(self.engine_info))
@@ -46,81 +41,40 @@ class Uci_worker(QObject):
                     if(bm):
                         move = bm.group()[9:]
                         self.emit(SIGNAL("bestmove(QString)"),move)
-            print("if2b ...")
             if(not self.command_queue.empty()):
-                print("if2c ...")
-
                 # first check if we are in go infinite mode
                 # then first send a stop command to engine
                 # before processing further commands
                 if(self.go_infinite):
                     self.process.write(bytes(("stop\n"),"utf-8"))
-                    print("if2d ...")
                     self.process.waitForBytesWritten()
-                    print("if2e ...")
                 msg = self.command_queue.get()
                 self.go_infinite = False
                 # if command is position fen moves, first count the
                 # numbers of moves so far to generate move numbers in engine info
-                print("if2cAAA ...")
-                #if(msg.startswith("position")):
-                    # there could be a lot of position + go infinite commands in the queue
-                    # if the user clicks very quickly through the game. then
-                    # check for the most _recent_ go infinite command in the
-                    # queue and forget about previous ones in order to get
-                    # quicker responses in the GUI for the user
-                    #finish = False
-                    #while(not finish):
-                    #    next_msg = None
-                    #    next_next_msg = None
-                    #    try:
-                    #        next_msg = self.command_queue.get(timeout=0.01)
-                    #    except queue.Empty:
-                    #        finish = True
-                    #    if not finish:
-                    #        try:
-                    #            next_next_msg = self.command_queue.get(timeout=0.01)
-                    #        except queue.Empty:
-                    #            finish = True
-                    #            self.command_queue.put(next_msg)
-                    #    if not finish:
-                    #        if next_msg.startswith("go infinite") and next_next_msg.startswith("position"):
-                    #            # just skip current position command and associated go infinite
-                    #            # and go to the last go infinite command
-                    #            msg = next_next_msg
-                    #        else:
-                    #            self.command_queue.put(next_next_msg)
-                    #            self.command_queue.put(next_msg)
-                    #    else:
-                    #        finish = True
-                    #match = self.MOVES.findall(msg)
-                    #if(len(match) > 0):
-                    #    self.engine_info.no_game_halfmoves = len(match)
-                print("if2c1 ...")
+                if(msg.startswith("position")):
+                    match = self.MOVES.findall(msg)
+                    if(len(match) > 0):
+                        self.engine_info.no_game_halfmoves = len(match)
                 if(msg.startswith("quit")):
                     self.process.write(bytes(("quit\n"),"utf-8"))
-                    print("if2f ...")
                     self.process.waitForBytesWritten()
                     self.process.waitForFinished()
-                    print("if2g ...")
                 elif(msg.startswith("go infinite")):
                     self.go_infinite = True
                     self.process.write(bytes(("go infinite\n"),"utf-8"))
-                    print("if2h ...")
                     self.process.waitForBytesWritten()
-                    print("if2i ...")
                 elif(msg.startswith("setoption name Skill Level")):
                     m = self.STRENGTH.search(msg)
                     if(m):
                         self.engine_info.strength = 1200+int(m.group()[18:])*100
                     self.process.write(bytes(msg+"\n","utf-8"))
                     self.process.waitForBytesWritten()
-                    print("if2j ...")
                 else:
                     self.process.write(bytes(msg+"\n","utf-8"))
                     self.process.waitForBytesWritten()
-                    print("if2kma ...")
 
+                
     def add_command(self,command):
         self.command_queue.put(command)
 
