b118d75a93d089fedccf22fd2ced260939c0e47a
asdfjkl <asdfjkl@users.noreply.github.com>
2016-07-23 11:32:59 +0200

    removed pyhton2 version
    Former-commit-id: 9007cce855d2caef4c65e886284c4c14a0847a80

--- util/crc32.py
+++
@@ -1,8 +0,0 @@
-import binascii
- 
-def crc32_from_file(filename):
-    buffer = open(filename,'rb')
-    buf = buffer.read()
-    buf = (binascii.crc32(buf) & 0xFFFFFFFF)
-    buffer.close()
-    return "%08X" % buf
--- various/windows_icons/make_icon.bat
+++
@@ -1,1 +0,0 @@
-png2ico.exe myicon.ico icon248.png icon48.png icon32.png icon16.png
