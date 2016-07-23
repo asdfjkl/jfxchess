73118f1f39db7da682ec69854684ca026581623e
asdfjkl <asdfjkl@users.noreply.github.com>
2016-07-23 11:32:59 +0200

    removed pyhton2 version
    Former-commit-id: 9007cce855d2caef4c65e886284c4c14a0847a80

--- README.md
+++
@@ -1,5 +0,0 @@
-This is the old prototype python version.
-For the current version and further info
-go to the new
-
-[project page](http://github.com/asdfjkl/jerry)
--- build_osx.sh
+++
@@ -1,6 +0,0 @@
-/opt/local/bin/python3.4 setup.py py2app --includes "sip"
-cp -R res dist/Jerry.app/Contents/Resources/
-cp -R books dist/Jerry.app/Contents/Resources/
-cp -R engine dist/Jerry.app/Contents/Resources/
-cp qt.conf dist/Jerry.app/Contents/Resources/qt.conf
-hdiutil create -volname Jerry -srcfolder ./dist -ov -format UDZO Jerry.dmg
