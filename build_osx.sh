8b5ac4692c4f304350b1ccc64a64952a8fb43b5d
Spill-The-Tea <44797905+Spill-The-Tea@users.noreply.github.com>
2019-04-06 03:06:33 -0700

    Update build_osx.sh

--- build_osx.sh
+++ build_osx.sh
@@ -1,9 +1,9 @@
-rm -r -f dist
-mkdir dist
-cp -R ../build-jerry3-Desktop_Qt_5_6_0_clang_64bit-Release/Jerry.app ./dist
-cp -R res ./dist/Jerry.app/Contents/Resources/
-cp -R engine ./dist/Jerry.app/Contents/Resources/
-cp -R books ./dist/Jerry.app/Contents/Resources/
-cp res/icons/osx_icon.icns ./dist/Jerry.app/Contents/Resources/
-cp Info.plist ./dist/Jerry.app/Contents/Info.plist
-../Qt/5.6/clang_64/bin/macdeployqt /Users/user/jerry/dist/Jerry.app -dmg
+mkdir jerry.app/Contents/Resources
+cp -R res jerry.app/Contents/Resources/
+cp -R data jerry.app/Contents/Resources/
+mkdir jerry.app/Contents/Resources/Books
+mkdir jerry.app/Contents/Resources/Databases
+mkdir jerry.app/Contents/Resources/Engines
+macdeployqt Jerry.app -dmg
+
+
