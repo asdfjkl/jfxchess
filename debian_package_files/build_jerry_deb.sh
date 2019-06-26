3ea0038731954137416929d8fcbc3ce004326f32
asdfjkl <user@host.com>
2019-06-26 23:15:27 +0200

    latest

---
+++ debian_package_files/build_jerry_deb.sh
@@ -0,0 +1,40 @@
+# _move this file two level up (dir's) before executing _
+# adjust: any source file
+# contents of src.pro
+# version name in dir's (jerry-3.2.0)
+#
+rm -r -f jerry
+git clone https://github.com/asdfjkl/jerry.git
+cd jerry_build
+rm -r -f jerry-3.2.0
+mkdir jerry-3.2.0
+cd jerry-3.2.0
+mkdir src
+mkdir debian
+cp ../../debian_files/COPYING ./COPYING
+cp ../../debian_files/jerry.desktop ./jerry.desktop
+cp ../../debian_files/README ./README
+cp ../../debian_files/jerry.pro ./jerry.pro
+cp ../../debian_files/src.pro ./src/src.pro
+cp -R ../../debian_files/debian/* ./debian
+cd src
+mkdir books
+mkdir chess
+mkdir controller
+mkdir dialogs
+mkdir model
+mkdir res
+mkdir uci
+mkdir various
+mkdir viewController
+cp ~/books/varied.bin ./books
+cp ../../../jerry/chess/*.* ./chess
+cp ../../../jerry/controller/*.* ./controller
+cp ../../../jerry/dialogs/*.* ./dialogs
+cp ../../../jerry/model/*.* ./model
+cp -R ../../../jerry/res ./res
+cp ../../../jerry/uci/*.* ./uci
+cp ../../../jerry/viewController/*.* ./viewController
+cp ../../../jerry/*.cpp ./
+cp ../../../jerry/*.h ./
+cp ../../../debian_package_files/LICENSE.TXT ./
