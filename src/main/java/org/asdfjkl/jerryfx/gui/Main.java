1560584a5282c320213d8eec3bf5ddb4df008740
asdfjkl <user@domain.invalid>
2020-10-14 09:11:21 +0200

    added file lock

--- src/main/java/org/asdfjkl/jerryfx/gui/Main.java
+++ src/main/java/org/asdfjkl/jerryfx/gui/Main.java
@@ -1,7 +1,44 @@
 package org.asdfjkl.jerryfx.gui;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+
 public class Main {
     public static void main(String[] args) {
+        String dir = System.getProperty("user.home");
+        File fDir = new File(dir);
+        File file = new File(fDir, "x43890423_jry.lock");
+        if(!lockInstance(file.getAbsolutePath())) {
+            return;
+        }
         App.main(args);
     }
+
+    private static boolean lockInstance(final String lockFile) {
+        try {
+            final File file = new File(lockFile);
+            final RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+            final FileLock fileLock = randomAccessFile.getChannel().tryLock();
+            if (fileLock != null) {
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    public void run() {
+                        try {
+                            fileLock.release();
+                            randomAccessFile.close();
+                            file.delete();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
