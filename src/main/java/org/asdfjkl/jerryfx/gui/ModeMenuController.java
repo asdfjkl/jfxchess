ee9ece3805dc3260c58c1217b67a7796c7715d99
asdfjkl <user@domain.invalid>
2021-03-15 09:24:42 +0100

    regression: game analysis mode (not: analysis mode) always with multipv 1

--- src/main/java/org/asdfjkl/jerryfx/gui/ModeMenuController.java
+++ src/main/java/org/asdfjkl/jerryfx/gui/ModeMenuController.java
@@ -216,7 +216,7 @@ public class ModeMenuController implements StateChangeListener {
         engineController.sendCommand("uci");
         engineController.sendCommand("ucinewgame");
         if(gameModel.activeEngine.supportsMultiPV()) {
-            engineController.sendCommand("setoption name MultiPV value "+gameModel.getMultiPv());
+            engineController.sendCommand("setoption name MultiPV value 1");
         }
         gameModel.setFlipBoard(false);
         gameModel.getGame().goToRoot();
